c6ef898c7022ba39ff85edda8090402c84214ce9
Kim-K <kim-k@localhost.localdomain>
2024-02-06 18:02:58 +0300

    changes

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:learning/constants/colors.dart';
 import 'package:learning/widgets/todo_item.dart';
+import 'package:learning/model/todo.dart';
+
 
 class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+  Home({Key? key}) : super(key: key);
+
+  final todosList = ToDo.todoList();
 
   @override
   Widget build(BuildContext context) {
@@ -28,16 +32,8 @@ class Home extends StatelessWidget {
                       ),
                     ),
                   ),
-                  ToDoItem(),
-                  ToDoItem(),
-                  ToDoItem(),
-                  ToDoItem(),
-                  ToDoItem(),
-                  ToDoItem(),
-                  ToDoItem(),
-                  ToDoItem(),
-                  ToDoItem(),
-                  ToDoItem(),
+                  for (ToDo todoo in todosList)
+                  ToDoItem(todo: todoo,),
                 ],
               ),
             )
