d5155de1384541863af3cfc01bbd36584050d287
Kim-K <kim-k@localhost.localdomain>
2024-02-06 18:32:17 +0300

    changes

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,7 +3,6 @@ import 'package:learning/constants/colors.dart';
 import 'package:learning/widgets/todo_item.dart';
 import 'package:learning/model/todo.dart';
 
-
 class Home extends StatelessWidget {
   Home({Key? key}) : super(key: key);
 
@@ -14,31 +13,71 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-        child: Column(
-          children: [
-            searchBox(),
-            Expanded(
-              child: ListView(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(top: 50, bottom: 20),
-                    child: Text(
-                      'My ToDo List',
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w500,
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: Column(
+              children: [
+                searchBox(),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 50, bottom: 20),
+                        child: Text(
+                          'My ToDo List',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
                       ),
+                      for (ToDo todoo in todosList)
+                        ToDoItem(
+                          todo: todoo,
+                        ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              children: [
+                Expanded(
+                    child: Container(
+                  margin: EdgeInsets.only(
+                    bottom: 20,
+                    right: 20,
+                    left: 20,
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: const [BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(0.0, 0.0),
+                      blurRadius: 10.0,
+                      spreadRadius: 0.0,
+                    ),],
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Add a new to do Item',
+                      border: InputBorder.none,
                     ),
                   ),
-                  for (ToDo todoo in todosList)
-                  ToDoItem(todo: todoo,),
-                ],
-              ),
-            )
-          ],
-        ),
+                )
+                )
+                
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
