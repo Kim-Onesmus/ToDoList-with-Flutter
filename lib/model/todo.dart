b38a9f526fa81e40561031e4d14346a4462bd8d6
Kim-K <kim-k@localhost.localdomain>
2024-02-06 17:47:13 +0300

    changes

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -13,10 +13,22 @@ class ToDo {
     return [
       ToDo(id: '01', todoText: 'Morning Excercise', isDone: true),
       ToDo(id: '02', todoText: 'Buy Groceries', isDone: true),
-      ToDo(id: '03', todoText: 'Check Emails', ),
-      ToDo(id: '04', todoText: 'Team Meeting', ),
-      ToDo(id: '05', todoText: 'Work on Djnago app', ),
-      ToDo(id: '06', todoText: 'Dinner with Nora', ),
-    ]
+      ToDo(
+        id: '03',
+        todoText: 'Check Emails',
+      ),
+      ToDo(
+        id: '04',
+        todoText: 'Team Meeting',
+      ),
+      ToDo(
+        id: '05',
+        todoText: 'Work on Djnago app',
+      ),
+      ToDo(
+        id: '06',
+        todoText: 'Dinner with Nora',
+      ),
+    ];
   }
 }
